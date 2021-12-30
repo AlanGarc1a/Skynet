1a99309755366db230c435e316cbc6bd5b7496c8
AlanGarc1a <alangarcia969@gmail.com>
2021-12-30 11:09:11 -0800

    add toggle props to sidebar and navbar components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from '../components/Header';
 import Section from '../components/Section';
 import About from '../components/About';
 import Services from '../components/Services';
 import Footer from '../components/Footer';
-import { CardDataOne} from '../components/Card/CardData';
+import SideBar from '../components/Sidebar';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+      setIsOpen(!isOpen)
+    }
+
     return (
         <>
+          <SideBar isOpen={isOpen} toggle={toggle}/>
+          <Navbar toggle={toggle}/>
           <Header />
           <About />
-          <Section {...CardDataOne} /> 
+          <Section  /> 
           <Services />
           <Footer /> 
         </>
