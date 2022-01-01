b3a98f07345fe6537c6c4f998c98c78140d56492
AlanGarc1a <alangarcia969@gmail.com>
2022-01-01 10:01:25 -0800

    removed margin and add color prop

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,12 +3,11 @@ import styled from "styled-components";
 const Button = styled.button`
     display: inline-block;
     font-size: 1.8rem;
-    border-radius: 0.3rem;
+    border-radius: 0.5rem;
     padding: 1rem 1rem;
-    margin: 0.5rem 1rem;
     width: 20rem;
     background: ${({primary}) => primary ? "#F9F7F7" : "#112D4E"};
-    color: #112D4E;
+    color: ${({primary}) => primary ? "#112D4E" : "#F9F7F7"};
     border: .2rem solid #F9F7F7;
     cursor: pointer;
 `;
