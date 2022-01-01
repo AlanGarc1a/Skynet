b7bc170c533fb885df40fdf7cbdef76c58842c85
AlanGarc1a <alangarcia969@gmail.com>
2022-01-01 10:19:45 -0800

    add product id to section and row prop component

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Container from '../Container/Container';
-import {Grid, Col, Row} from '../Grid/Grid';
-import { SectionWrapper, ImgWrapper, SectionContent, 
-    SectionTitle, 
+import { Grid, Col, Row } from '../Grid/Grid';
+import {
+    SectionWrapper, ImgWrapper, SectionContent,
+    SectionTitle,
     SectionAbout,
     Img,
-    Title, 
-SectionDescription } from './SectionElements';
+    Title,
+    SectionDescription
+} from './SectionElements';
 import RobotImg from '../../assets/robot.jpg';
 import MilitaryImg from '../../assets/military.jpg';
 import AIImg from '../../assets/AI.jpg';
@@ -14,7 +16,7 @@ import Block from "../Block/Block";
 
 const index = () => {
     return (
-        <SectionWrapper>
+        <SectionWrapper id="products">
             <Container>
                 <Title>What Do We Do?</Title>
                 <Block primary />
@@ -30,30 +32,30 @@ const index = () => {
                                 <SectionTitle>Number #1 leader in Cybernetic Organisms</SectionTitle>
                                 <SectionAbout>
                                     <SectionDescription>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-                                        od tempor incididunt ut labore et dolore magna aliqua. Ut enim ad 
-                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-                                        aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        od tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
                                         culpa qui officia deserunt mollit anim id est laborum.
                                     </SectionDescription>
                                 </SectionAbout>
                             </SectionContent>
                         </Col>
                     </Row>
-                    <Row>
+                    <Row wrap="true">
                         <Col>
-                        <SectionContent>
+                            <SectionContent>
                                 <SectionTitle>Number #1 leader in Military Technology</SectionTitle>
                                 <SectionAbout>
                                     <SectionDescription>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-                                        od tempor incididunt ut labore et dolore magna aliqua. Ut enim ad 
-                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-                                        aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        od tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
                                         culpa qui officia deserunt mollit anim id est laborum.
                                     </SectionDescription>
                                 </SectionAbout>
@@ -76,12 +78,12 @@ const index = () => {
                                 <SectionTitle>Number #1 leader in Artificial Intelligence</SectionTitle>
                                 <SectionAbout>
                                     <SectionDescription>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-                                        od tempor incididunt ut labore et dolore magna aliqua. Ut enim ad 
-                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-                                        aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        od tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
                                         culpa qui officia deserunt mollit anim id est laborum.
                                     </SectionDescription>
                                 </SectionAbout>
